fd114f3ab38d95adcfc6571663b175fdd2112f05
Rob Hoelz <rob@hoelz.ro>
2016-01-09 10:31:50 -0600

    Intial comit

---
+++ lib/Pod/DeclarationsAtEnd.pm
@@ -0,0 +1,29 @@
+use v6;
+unit class Pod::DeclarationsAtEnd;
+
+
+=begin pod
+
+=head1 NAME
+
+Pod::DeclarationsAtEnd - blah blah blah
+
+=head1 SYNOPSIS
+
+  use Pod::DeclarationsAtEnd;
+
+=head1 DESCRIPTION
+
+Pod::DeclarationsAtEnd is ...
+
+=head1 AUTHOR
+
+ <>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2016 
+
+This library is free software; you can redistribute it and/or modify it under the Artistic License 2.0.
+
+=end pod
---
+++ t/01-basic.t
@@ -0,0 +1,7 @@
+use v6;
+use Test;
+use Pod::DeclarationsAtEnd;
+
+pass "replace me";
+
+done-testing;
