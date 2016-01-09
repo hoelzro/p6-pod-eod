77c8595ef7c036c10b2d36f4c2a87715d94b463e
Rob Hoelz <rob@hoelz.ro>
2016-01-09 10:32:14 -0600

    Add initial implementation

--- lib/Pod/DeclarationsAtEnd.pm
+++ lib/Pod/DeclarationsAtEnd.pm
@@ -1,29 +1,5 @@
 use v6;
-unit class Pod::DeclarationsAtEnd;
 
-
-=begin pod
-
-=head1 NAME
-
-Pod::DeclarationsAtEnd - blah blah blah
-
-=head1 SYNOPSIS
-
-  use Pod::DeclarationsAtEnd;
-
-=head1 DESCRIPTION
-
-Pod::DeclarationsAtEnd is ...
-
-=head1 AUTHOR
-
- <>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2016 
-
-This library is free software; you can redistribute it and/or modify it under the Artistic License 2.0.
-
-=end pod
+our sub move-declarations-to-end($pod) is export {
+    $pod[0..*] = $pod.sort(*.?WHEREFORE.Bool).eager;
+}
